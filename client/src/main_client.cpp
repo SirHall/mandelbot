608c1c44dd42323e22cdd6a27868221e27a244c4
SirHall <sirhallstein@gmail.com>
2020-03-11 23:03:05 +1000

    Feed RendererMandelbrot test data from main_client

--- client/src/main_client.cpp
+++ client/src/main_client.cpp
@@ -2,21 +2,32 @@
 #include <iostream>
 #include <volt/net.hpp>
 
-namespace mp = boost::multiprecision;
-
-typedef mp::number<mp::mpfr_float_backend<1000>> lrg_float;
+#include "RendererMandelbrot.hpp"
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
-    {
-        std::cout << "Usage: client [host] [port]" << std::endl;
-        std::exit(0);
-    }
+    // if (argc < 3)
+    // {
+    //     std::cout << "Usage: client [host] [port]" << std::endl;
+    //     std::exit(0);
+    // }
 
-    auto network = volt::net::network::create(2, true);
+    // auto network = volt::net::network::create(2, true);
 
-    network->server_connect(argv[1], argv[2]);
+    // network->server_connect(argv[1], argv[2]);
+
+    auto settings                 = RenderSettings();
+    settings.algorithmID          = 0;
+    settings.done                 = false;
+    settings.renderWidth          = 4096;
+    settings.renderHeight         = 2160;
+    settings.milliSecondsPerFrame = 17;
+    settings.totalFrames          = 10000;
+
+    auto mandelRenderer = RendererMandelbrot(settings);
+
+    auto image = mandelRenderer.Draw(500);
+    image.SaveAs("10.png");
 
     return 0;
 }
