2e903c0aa5588781b8871db561df3bdc69cc581a
SirHall <sirhallstein@gmail.com>
2020-03-11 22:59:58 +1000

    Remove implementations from FrameData header

--- shared/include/FrameData.hpp
+++ shared/include/FrameData.hpp
@@ -15,16 +15,9 @@ public:
 
 template <>
 void volt::net::serialize::write_into(struct FrameData const &v,
-                                      message_ptr &           data)
-{
-    volt::net::serialize::write_into(v.frameNumber, data);
-}
+                                      message_ptr &           data);
 
 template <>
 void volt::net::deserialize::read_into(message_iter &    iterator,
-                                       struct FrameData &instance)
-{
-    volt::net::deserialize::read_into(iterator, instance.frameNumber);
-}
-
+                                       struct FrameData &instance);
 #endif
